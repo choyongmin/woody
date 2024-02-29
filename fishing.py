30e38493d720717c5a001d3d4e1a49fbcc3aa039
joyongmin <whdydals97@naver.com>
2024-02-29 12:26:45 -0600

    random blink

--- fishing.py
+++ fishing.py
@@ -15,6 +15,19 @@ needfix = 0
 # 688>793 H
 # 1550 > 1650 Width
 
+def find_color_on_screen(expected_color):
+    # Capture the screen
+    screenshot = pag.screenshot()
+    # Check for the specific color on the entire screen
+    for x in range(screenshot.width):
+        for y in range(screenshot.height):
+            actual_color = screenshot.getpixel((x, y))
+            if actual_color == expected_color:
+                return x, y
+    return None, None
+
+
+
 def compute_icon_type(img):
     mean = np.mean(img, axis=(0, 1))
     r, g, b = mean[0], mean[1], mean[2]
@@ -44,28 +57,52 @@ def click(coords):
     pag.mouseUp()
 
 
-while True:
-    with mss.mss() as sct:
-        pop_img = np.array(sct.grab(check))[:, :, :3]
-        need_start = np.array(sct.grab(start_position))[:, :, :3]
-        waring = np.array(sct.grab(Warning))[:, :, :3]
-        blink = compute_icon_type(pop_img)
-        st = compute_icon_type(need_start)
-        wn = compute_icon_type(waring)
+def main():
+    # Expected color (put your RGB value here)
+    expected_color = (0, 230, 250)  # Example: (0, 230, 250)
+
+    # Set the click distance from the detected color position (in pixels)
+    click_distance_x = 190  # Example: 20 pixels from x axis
+    click_distance_y = 120  # Example: 20 pixels from y axis
+
+    # Set the range of random clicks around the detected color position (in pixels)
+    random_click_range = 30  # Example: 10 pixels
+    while True:
+        with mss.mss() as sct:
+            pop_img = np.array(sct.grab(check))[:, :, :3]
+            need_start = np.array(sct.grab(start_position))[:, :, :3]
+            waring = np.array(sct.grab(Warning))[:, :, :3]
+            blink = compute_icon_type(pop_img)
+            st = compute_icon_type(need_start)
+            wn = compute_icon_type(waring)
+        # If the specific color is found on the screen
+        target_x, target_y = find_color_on_screen(expected_color)
+        if target_x is not None and target_y is not None:
+            # Calculate random click position within the specified range
+            random_x = random.randint(target_x - random_click_range, target_x + random_click_range)
+            random_y = random.randint(target_y - random_click_range, target_y + random_click_range)
+
+            # Calculate the click position within the specified distance from the detected color position
+            click_x = min(max(random_x, target_x - click_distance_x), target_x + click_distance_x)
+            click_y = min(max(random_y, target_y - click_distance_y), target_y + click_distance_y)
+            here = (click_x,click_y)
+            # Perform the click
+            click(here)
+
         #print(blink)
         #print(wn)
         print(st)
 
 
-    
+
         if st =='need start':
             click(fishing)
         elif st =='black' :
             click(fishing)
 
-        if blink == 'blink':
-            print('tab!!!')
-            click(fishing)
+        #if blink == 'blink':
+         #   print('tab!!!')
+          #  click(fishing)
 
         if wn == 'yellow':
             print("we have to recharge")

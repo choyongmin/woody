adaf6c9f14dc8ff983dd767a02e8dbdb6b4746dd
joyongmin <whdydals97@naver.com>
2024-03-02 14:01:43 -0600

    test

--- fishing.py
+++ fishing.py
@@ -11,11 +11,7 @@ check = {'left': 1563, 'top': 759, 'width': 5, 'height': 5} #blinkimage
 start_position = {'left': 1747, 'top': 853, 'width': 5, 'height': 5} #낚시대버튼
 pag.PAUSE = 0.08
 needfix = 0
-expected_color = (86, 172, 152)  # 낚시버튼(86,172,152)
-click_distance_x = 190  # Example: 20 pixels from x axis
-click_distance_y = 120  # Example: 20 pixels from y axis
-# 688>793 H
-random_click_range = 10  # Example: 10 pixels
+expected_color = (248, 239, 80)  # 낚시버튼(86,172,152)
 # 1550 > 1650 Width
 
 def compute_icon_type(img):
@@ -61,43 +57,37 @@ def find_color_on_screen(expected_color):
 while True:
     try:
         with mss.mss() as sct:
-            pop_img = np.array(sct.grab(check))[:, :, :3]
-            need_start = np.array(sct.grab(start_position))[:, :, :3]
-            waring = np.array(sct.grab(Warning))[:, :, :3]
-            blink = compute_icon_type(pop_img)
-            st = compute_icon_type(need_start)
-            wn = compute_icon_type(waring)
+#           pop_img = np.array(sct.grab(check))[:, :, :3]
+#           need_start = np.array(sct.grab(start_position))[:, :, :3]
+#           waring = np.array(sct.grab(Warning))[:, :, :3]
+#           blink = compute_icon_type(pop_img)
+#           st = compute_icon_type(need_start)
+#           wn = compute_icon_type(waring)
         #If the specific color is found on the screen
             target_x, target_y = find_color_on_screen(expected_color)
             print( "target :" , target_x,target_y )
 
-        if st =='need start':
-            click(fishing)
-        elif st =='black' :
-            click(fishing)
-        if blink == 'blink':
+#       if st =='need strt':
+#           click(fishing)
+#       elif st =='black' :
+#           click(fishing)
+#       if blink == 'blink':
+
           #  print('tab!!!')
              #click(fishing)
+            # Calculate the click position within the specified distance from the detected color position
+            here = (target_x+80,target_y+60)
+            # Perform the click
+            print("target: ",target_x,target_y)
+            print("real tap ",here)
+            #if target_x != 0 and target_y != 0:
+                #click(here)
 
-        #if target_x is not None and target_y is not None:
-                # Calculate random click position within the specified range
-                random_x = random.randint(target_x - random_click_range, target_x + random_click_range)
-                random_y = random.randint(target_y - random_click_range, target_y + random_click_range)
-
-                # Calculate the click position within the specified distance from the detected color position
-                click_x = min(max(random_x, target_x - click_distance_x), target_x + click_distance_x)
-                click_y = min(max(random_y, target_y - click_distance_y), target_y + click_distance_y)
-                here = (target_x+80,target_y+60)
-                # Perform the click
-                print("target: ",target_x,target_y)
-                print("sibal ",here)
-                click(here)
-
-        if wn == 'yellow':
-            print("we have to recharge")
-            click(confirm)
-            with open('./needfix.txt' ,'w') as file:
-                file.write('1')
+#       if wn == 'yellow':
+#           print("we have to recharge")
+#           click(confirm)
+#           with open('./needfix.txt' ,'w') as file:
+#               file.write('1')
             break
     except pag.ImageNotFoundException:
         print("이미지를 찾지 못했습니다. 다시 시도합니다.")
